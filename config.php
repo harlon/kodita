bc5768e9f0e3e23980acc8d76b4f96e4acc7214e
Francisco <fescobra@everis.com>
2019-02-22 22:52:57 -0300

    prueba login

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ class Config {
         // Data Source Name, contains the information required to connect to the database
         'dsn'      => 'mysql:host=localhost;dbname=koda_posting;charset=utf8',
         'username' => 'root',
-        'password' => 'root'
+        'password' => ''
     );
     
     public static $SEND_EMAILS = true;
